07f43f66e18f2c8575f985e10e847f6495695ec1
Jonas Odden <oddenjonas1@gmail.com>
2024-01-27 21:38:48 +0100

    Implemented dialogue box and start of court

---
+++ Assets/Scripts/CourtManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jestering.Rating
+{
+    public class CourtManager : MonoBehaviour
+    {
+        public void StartPresent()
+        {
+            
+        }
+    }
+}
